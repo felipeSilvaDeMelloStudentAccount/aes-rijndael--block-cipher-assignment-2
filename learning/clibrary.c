c7b7930d370b69b8b1d75c25d5c3bb1f8848fbbe
felipeSilvaDeMelloStudentAccount <D23125661@mytudublin.ie>
2024-03-17 12:05:04 +0000

    Learning Python CTypes

--- learning/clibrary.c
+++ learning/clibrary.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-void display(){
-    printf("Hello World");
+char* display(char* str, int age){
+    printf("My Name is %s and my age is %d", str, age);
+    return "Completed";
 }
+
+// We need to to compile this file with the following command:
+// gcc -fPIC -shared -o clibrary.so clibrary.c 
+// we are using  -fPIC for position independent code, 
+// -shared to create a shared library 
+// -o to specify the output file name
+
+
+
+
+// Ref : Python ctypes Tutorial - Using C/C++ Functions in Python
+// https://youtu.be/neexS0HK9TY?si=SE1xH6Jxe-0Z1tNU
